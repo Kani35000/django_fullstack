eca0f23efecec0d532d378554545925eb11a2068
Kanidaye Okorji <kanidayeokorji@gmail.com>
2023-04-10 14:57:04 +0200

    django-Reeact commit

---
+++ music_cordinator/frontend/src/components/App.py
@@ -0,0 +1,14 @@
+import React, { component } from "react";
+import {render} from "react-dom";
+
+export default class App extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return <h1>Testing React code</h1>
+    }
+}
+
+const appDiv = document.getElementbyId("app");
+render(<App/>, appDiv);
